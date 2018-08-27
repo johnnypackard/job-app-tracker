5d0155748f8b8e744cd62bb735a3f7accbc1af30
Johnny Packard <packard81@me.com>
2018-08-27 16:57:34 -0500

    added cookie middleware

---
+++ server/modules/session-middleware.js
@@ -0,0 +1,24 @@
+const cookieSession = require('cookie-session');
+const warnings = require('../constants/warnings');
+
+// user can enter username and password one time and stay logged in
+
+const serverSessionSecret = () => {
+    if (!process.env.SERVER_SESSION_SECRET ||
+        process.env.SERVER_SESSION_SECRET.length < 8 ||
+        process.env.SERVER_SESSION_SECRET === warnings.exampleBadSecret) {
+
+            // if user doesn't have a good secret, gives warning
+            console.log(warnings.badSecret);
+    }
+    
+    return process.env.SERVER_SESSION_SECRET;
+};
+
+module.exports = cookieSession({
+    secret: serverSessionSecret() || 'secret', // as defined in .env file
+    key: 'user',
+    resave: 'false',
+    saveUninitialized: false,
+    cookie: { maxage: 60000, secure: false },
+});
