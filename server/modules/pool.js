d94b9367cb5664eff5818b7a2cc6579858d1f0c1
Johnny Packard <packard81@me.com>
2018-08-27 16:50:21 -0500

    adding pool module for db

---
+++ server/modules/pool.js
@@ -0,0 +1,44 @@
+const pg = require('pg');
+const url = require('url');
+
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    };
+} else { 
+    config = {
+        host: 'localhost',
+        port: 5432,
+        database: 'jobs_tracker',
+        max: 10,
+        idleTimeoutMills: 30000,
+    };
+}
+
+// sharing pool on all modules
+const pool = new pg.Pool(config);
+
+// connected to db
+pool.on('connect', () => {
+    console.log('pg connected');
+});
+
+// if clients are idle, error
+pool.on ('error', (err) => {
+    console.log('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
+module.exports = pool;
