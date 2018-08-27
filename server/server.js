a98bb436d9eaeafa52bf3c5587d6db48cdea0bbd
Johnny Packard <packard81@me.com>
2018-08-27 16:03:28 -0500

    adding server folder set up

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');
+const sessionMiddleware = require('./modules/session-middleware');
+
+const passport = require('./strategies/user.strategy');
+
+const userRouter = require('./routes/user.router');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport session config
+app.use(sessionMiddleware);
+
+// start Passport sessions
+app.use(passpor.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/api/user', userRouter);
+
+// serve static files
+app.use(express.static('build'));
+
+// Server port
+const PORT = process.env.PORT || 5000;
+
+// spin up server
+app.listen(PORT, () => {
+    console.log(`spinning server on port: ${PORT}`); 
+});
