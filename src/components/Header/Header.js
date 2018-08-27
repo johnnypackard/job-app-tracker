d4b39dde9188e9a16463e5a56b88550931366a4f
Johnny Packard <packard81@me.com>
2018-08-27 18:27:34 -0500

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({ title }) => (
+    <div className="header">
+        <div>
+            <h1>{ title }</h1>    
+        </div>
+    </div>
+);
+
+export default Header;
