c4f3f61514e15ae144ee7d01a39cc5b6f4074c05
Johnny Packard <packard81@me.com>
2018-08-27 17:01:29 -0500

    added warning regarding env bcrypt

---
+++ server/constants/warnings.js
@@ -0,0 +1,24 @@
+const badSecret = `
+
+--------------------------
+
+*** WARNING ***
+Your application is not very secure.
+You need to set SERVER_SESSION_SECRET to a better secret
+Please follow the README and add a .env file
+
+It should be
+- longer than 8 characters
+- not "superDuperSecret"
+
+If this warning is showing on you deployed app,
+add or change your SERVER_SESSION_SECRET environment variable!
+
+--------------------------`;
+
+const exampleBadSecret = 'superDuperSecret';
+
+module.exports = {
+    badSecret,
+    exampleBadSecret,
+};
