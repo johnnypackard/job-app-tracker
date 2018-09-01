d6f08d868f8f22460328b5af386ba70703e297fe
Johnny Packard <packard81@me.com>
2018-09-01 09:42:15 -0500

    added info page, login page, and nav

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 *Needs to be done ASAP*
 
-[] add InfoPage component 
-[] add LoginPage component 
-[] add Nav component 
+[x] add InfoPage component 
+[x] add LoginPage component 
+[x] add Nav component 
 [x] add RegisterPage component 
 [] add UserPage component 
---
+++ src/components/InfoPage/InfoPage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Nav from '../Nav/Nav';
+import { USER_ACTIONS } from '../../redux/actions/userActions';
+
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+class InfoPage extends Component {
+    componentDidMount() {
+        this.props.dispatch({type: USER_ACTIONS.FETCH_USER});
+    }
+
+    componentDidUpdate() {
+        if (!this.props.user.isLoading && this.props.user.userName === null) {
+            this.props.history.push('home');
+        }
+    }
+
+    render() {
+        let content = null;
+
+        if (this.props.user.userName) {
+            content = (
+                <div>
+                    <p>
+                        Info Page
+                    </p>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <Nav />
+                { content }
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps)(InfoPage);
